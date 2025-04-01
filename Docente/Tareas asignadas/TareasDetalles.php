73eb2dcfc62375bd45e8b3d6df660d83bc4738c2
PanBimbo16 <alejandrogaleanocanas@gmail.com>
2025-04-01 13:41:40 -0500

    desarrollo detalles tareas

---
+++ Docente/Tareas asignadas/TareasDetalles.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalles de Tarea</title>
+    <?php include "../../SIDEBAR/Docente/head.php" ?>
+    <link rel="stylesheet" href="tareascss.css">
+</head>
+<body>
+<?php include "../../SIDEBAR/Docente/sidebar.php" ?>
+<section class="home">
+        <div class="main-content">
+            <div class="header">
+                <h1 id="titulo1-header">DETALLES DE TAREAS</h1>
+                <div class="profile">
+                <i class="fa-regular fa-bell"></i>
+                    <div class="profile-info">
+                        <div><h3>Antonio</h3></div>
+                        <div><p>Docente de Matemáticas</p></div>
+                    </div>
+                    <a href="../UserProfile/index.php"><i class="fa-solid fa-user"></i></a>
+                </div>
+            </div>
+</section>
+</body>
+</html>
