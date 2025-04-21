a8621c867409f1a08400b008cb6c726bec02b5c6
Nico08ben <armeronico2008@gmail.com>
2025-04-21 10:52:18 -0500

    terminado el inicio de sesion ya funcional toño te quiero mucho xd armero mi dios

--- Inicio/Administrador/index.php
+++ Inicio/Administrador/index.php
@@ -9,16 +9,15 @@
 </head>
 
 <body>
-    <header></header>
     <main>
         <div class="container">
             <div class="Login-container">
                 <h1 id="Titulo-Login">Iniciar Sesión</h1>
                 <p id="Subtitulo-login">ADMINISTRADORES</p>
                 <form class="Login" action="../../validar_login.php" method="POST">
-                    <p id="p-usuario">Usuario</p>
+                    <label for="campo-usuario" id="p-usuario">Usuario</label>
                     <input id="campo-usuario" type="text" name="usuario" placeholder="Usuario" required>
-                    <p id="p-contraseña">Contraseña</p>
+                    <label for="campo-contraseña" id="p-contraseña">Contraseña</label>
                     <input id="campo-contraseña" type="password" name="password" placeholder="Contraseña" required>
                     <button id="button-login" type="submit">INICIAR</button>
                 </form>
@@ -28,12 +27,11 @@
                 <p id="p-docente">Inicia sesión en nuestro apartado de Docentes para poder ingresar a nuestra página web
                     en modo Docentes.</p>
                 <a href="../../index.php">
-                    <button id="button-docente">Iniciar Sesión</button>
+                    <button id="button-docente">Iniciar Sesion</button>
                 </a>
             </div>
         </div>
     </main>
-    <footer></footer>
 </body>
 
 </html>
