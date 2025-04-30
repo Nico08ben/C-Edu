351fd9319094cd0d183a6efc4497e6f710bb4a54
khazix4444 <hombredivinoqueactuacomoperro@gmail.com>
2025-04-30 11:05:01 -0500

    Add files via upload

--- cedu.sql
+++ cedu.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-04-2025 a las 22:36:13
+-- Tiempo de generación: 30-04-2025 a las 18:04:30
 -- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -185,7 +185,7 @@ CREATE TABLE `tarea` (
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
   `email_usuario` varchar(100) NOT NULL,
-  `contraseña_usuario` varchar(255) NOT NULL,
+  `contrasena_usuario` varchar(255) NOT NULL,
   `nombre_usuario` varchar(100) NOT NULL,
   `telefono_usuario` varchar(15) DEFAULT NULL,
   `id_institucion` int(11) DEFAULT NULL,
@@ -198,7 +198,7 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `email_usuario`, `contraseña_usuario`, `nombre_usuario`, `telefono_usuario`, `id_institucion`, `id_rol`, `id_materia`, `grupo_cargo_usuario`) VALUES
+INSERT INTO `usuario` (`id_usuario`, `email_usuario`, `contrasena_usuario`, `nombre_usuario`, `telefono_usuario`, `id_institucion`, `id_rol`, `id_materia`, `grupo_cargo_usuario`) VALUES
 (3, 'admin@cedu.com', '$2y$10$AeM0rWoEu8Ma1H/hs8Xw/uEkndmBrOL9zNI0rNHw.gR9bVxe2sJ4S', 'Administrador Principal', '', 1, 0, NULL, NULL),
 (4, 'maestro@cedu.com', '$2y$10$lLNqY/cEfhJyAuXaxhOJ8OyumVrA434f5Ifp1uzTxbz0nKhkhwNpe', 'Maestro Ejemplo', '', 1, 1, NULL, NULL),
 (6, 'juliancho@gmail.com', '$2y$10$vhMQvQXhjPC8Nn3TKUCS4OHj9TjjYSiZhGfKA3t4U/RxnC0w5LJzS', 'Julian Ospina', '2433232323', 1, 1, 4, NULL);
